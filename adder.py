ad400157592f7980d1e264e620be1d2a08826d94
ludaya <ludaya201@gmail.com>
2016-04-22 16:57:19 +0900

    사칙연산 구현

--- adder.py
+++ adder.py
@@ -1,5 +1,8 @@
-print(**********ADDER**********)
+print(**********CALC**********)
 a = (int)(input('Input 1st Number: '))
 b = (int)(input('Input 2nd Number: '))
-print("%d + %d = %d" % (a,b,a+b))
-print(**********ADDER**********)
+print("%d + %d = %d" % (a,b,a+b)) # add
+print("%d - %d = %d" % (a,b,a-b)) # subtract
+print("%d * %d = %d" % (a,b,a*b)) # product
+print("%d / %d = %d" % (a,b,a/b)) # divide
+print(**********CALC**********)
