6ccba5e95d9041c630615766e91547d082256a2d
ludaya <ludaya201@gmail.com>
2016-04-22 16:51:27 +0900

    2nd commit modified adder.py

--- adder.py
+++ adder.py
@@ -1,3 +1,5 @@
+print(**********ADDER**********)
 a = (int)(input('Input 1st Number: '))
 b = (int)(input('Input 2nd Number: '))
-print("%d + %d = %d" % (a,b,a+b)) 
+print("%d + %d = %d" % (a,b,a+b))
+print(**********ADDER**********)
